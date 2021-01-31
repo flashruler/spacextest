a8c35ebbf16c50a7a3ce1ec18bf058ae578357e2
flashruler <44584657+flashruler@users.noreply.github.com>
2021-01-30 22:22:47 -0800

    first plunge

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+const axios = require('axios');
+
+
+class App extends React.Component {
+  state = {
+    launches: []
+  }
+  constructor(props) {
+    super(props)
+    this.getData = this.getData.bind(this)
+
+  }
+  componentDidMount() {
+    this.getData()
+  }
+  getData() {
+    let that = this
+    axios.get('https://api.spacexdata.com/v4/launches/upcoming')
+      .then(function (response) {
+        console.log(response.data);
+        that.setState({ launches: response.data })
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  }
+  render() {
+    return (
+      <div className='page'>
+        <table>
+          <thead>
+            <tr>
+              <th>Flight#</th>
+              <th>Name</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.launches && this.state.launches.map((value, index) => {
+              return (
+                <tr key={index}>
+                  <td>{value.flight_number}</td>
+                  <td>{value.name}</td>
+                  <td>{value.date_local}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+
+}
+
+export default App;
