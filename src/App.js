b7fe72fc2eb0fd8d02803df7f6556aa1e42786ad
flashruler <44584657+flashruler@users.noreply.github.com>
2021-02-02 00:05:35 -0800

    Added CSS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from "react";
 const axios = require('axios');
@@ -6,15 +5,29 @@ const axios = require('axios');
 
 class App extends React.Component {
   state = {
-    launches: []
+    launches: [],
+    showInfo: -1
   }
   constructor(props) {
     super(props)
     this.getData = this.getData.bind(this)
-
+    this.handleClick = this.handleClick.bind(this)
+    this.getBooster = this.getBooster(this)
   }
   componentDidMount() {
     this.getData()
+  }
+  getBooster(core)
+  {
+    console.log(core)
+    axios.get('https://api.spacexdata.com/v4/cores/'+core)
+      .then(function (response) {
+        return response.data.serial
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
   }
   getData() {
     let that = this
@@ -22,35 +35,46 @@ class App extends React.Component {
       .then(function (response) {
         console.log(response.data);
         that.setState({ launches: response.data })
+        
       })
       .catch(function (error) {
         // handle error
         console.log(error);
       })
   }
+  handleClick(index){
+    if(index===this.state.showInfo)
+    index=-1
+    this.setState(state=>({showInfo: index}))
+  }
   render() {
     return (
+
       <div className='page'>
-        <table className= "table">
-          <thead>
-            <tr>
-              <th>Flight#</th>
-              <th>Name</th>
-              <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.launches && this.state.launches.map((value, index) => {
-              return (
-                <tr key={index}>
-                  <td>{value.flight_number}</td>
-                  <td>{value.name}</td>
-                  <td>{value.date_local}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+
+        {this.state.launches && this.state.launches.map((value, index) => {
+          if (index<2) {
+            const divStyle = { backgroundImage: 'url(https://live.staticflickr.com/65535/49635401403_96f9c322dc_o.jpg)' }
+            return (
+              <div className='box' key={index} >
+                <div className='launch' onClick= {()=>this.handleClick(index)} style={divStyle}>
+                  <h1>{value.name}</h1>
+                  <p className='paragraph'>{value.details}</p>
+                </div>
+                {this.state.showInfo === index &&
+                  <div className='info'>
+                    <h1> Launch Info</h1>
+                    <h3>Flight No. : {value.flight_number}</h3>
+                    <h3>Date: {value.date_local}</h3>
+                    {/* <h2>Booster No. : {()=>this.getBooster(value.cores[0].core)}</h2> */}
+                    {console.log(value.cores[0].core)}
+                  </div>} 
+              </div>
+            )
+          }
+
+        })}
+
       </div>
     );
   }
